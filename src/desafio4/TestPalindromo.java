293e608eb48cabf04844e37d71bdb270dbc7f3c4
vanessabmdf <vanessabmdf@gmail.com>
2017-10-19 23:12:42 -0300

    Teste para as funcionalidades de inversao e palindromia

---
+++ src/desafio4/TestPalindromo.java
@@ -0,0 +1,38 @@
+package desafio4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPalindromo {
+
+	@Test
+	public void testInversaoDeString() {
+		String stringDeTeste = "ame a ema";
+		
+		assertEquals(stringDeTeste, Palindromo.stringInversa(stringDeTeste));
+	}
+	
+	@Test
+	public void testInversaoDeStringNaoPalindroma() {
+		String stringDeTeste = "vanessa";
+		String inversao = "assenav";
+		
+		assertEquals(inversao, Palindromo.stringInversa(stringDeTeste));
+		assertNotEquals(stringDeTeste, Palindromo.stringInversa(stringDeTeste));
+	}
+	
+	@Test
+	public void testNumeroNaoPalindromo() {
+		int numeroTeste = 1234;
+		
+		assertFalse(Palindromo.numeroPalindromo(numeroTeste));
+	}
+	
+	@Test
+	public void testNumeroPalindromo() {
+		int numeroTeste = 9009;
+		
+		assertTrue(Palindromo.numeroPalindromo(numeroTeste));
+	}
+}
