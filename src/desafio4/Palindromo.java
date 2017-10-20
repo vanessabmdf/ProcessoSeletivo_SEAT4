1c5d170c0914e0d5214a41804117014e8c8d0f32
vanessabmdf <vanessabmdf@gmail.com>
2017-10-19 22:57:15 -0300

    Metodo para verificar numero palindromo

--- src/desafio4/Palindromo.java
+++ src/desafio4/Palindromo.java
@@ -3,6 +3,27 @@ package desafio4;
 public class Palindromo {
 	
 	 public static void main(String[] args) {
-	
+		
+		 }
+	 
+	 
+	 public static boolean numeroPalindromo(int numeroTestado){
+		 String numero = Integer.toString(numeroTestado);
+		 String numeroInverso = stringInversa(numero);
+		 
+		 if(numero.equalsIgnoreCase(numeroInverso))
+			 return true;
+		 
+		 return false;
+	 }
+	 
+	 public static String stringInversa(String stringParaInversao){
+		 String stringInversa = new String();
+		 
+		 for (int i = stringParaInversao.length()-1; i >=0 ; i--) {
+			stringInversa = stringInversa+stringParaInversao.charAt(i);
+		}
+		 
+		 return stringInversa;
 	 }
 }
